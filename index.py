5061262b2ed01d261e77626657350930e1260507
Hec98 <hector982015@gmail.com>
2021-10-30 22:34:25 -0500

    Query data

--- index.py
+++ index.py
@@ -2,7 +2,8 @@ from tkinter import Tk, LabelFrame, Label, Entry, Button, ttk
 import sqlite3 
 
 class Product:
-    def __init__(self,window):
+    db_name = 'database.db'
+    def __init__(self, window):
         self.wind = window
         self.wind.title('Products Application')
 
@@ -29,6 +30,29 @@ class Product:
         self.tree.grid(row = 4, column = 0, columnspan = 2)
         self.tree.heading('#0', text = 'Name')
         self.tree.heading('#1', text = 'Price')
+
+        self.get_products()
+    
+    def run_query(self, query, parameters = ()):
+        with sqlite3.connect(self.db_name) as conn:
+            cursor = conn.cursor()
+            result = cursor.execute(query, parameters)
+            conn.commit()
+        return result
+
+    def get_products(self):
+        # Cleaning table
+        records = self.tree.get_children()
+        for element in records: self.tree.delete(element)
+
+        # Query data
+        query = 'SELECT * FROM product ORDER BY name DESC'
+        db_rows = self.run_query(query)
+
+        # Fulling data
+        for row in db_rows:
+            self.tree.insert('', 0, text = row[1], values = row[2])
+
 
 if __name__ == '__main__':
     window = Tk()
