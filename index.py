3dce03f165dd9b34264467f51d996f5adde1b630
Hec98 <hector982015@gmail.com>
2021-11-06 22:33:18 -0600

    Save data in database

--- index.py
+++ index.py
@@ -23,7 +23,7 @@ class Product:
         self.price.grid(row = 2, column = 1)
 
         # Button Add Product
-        ttk.Button(frame, text = 'Save Product').grid(row = 3, columnspan = 2, sticky = 'W E')
+        ttk.Button(frame, text = 'Save Product', command = self.add_product).grid(row = 3, columnspan = 2, sticky = 'W E')
 
         # Table
         self.tree = ttk.Treeview(height = 10, columns = 2)
@@ -52,7 +52,19 @@ class Product:
         # Fulling data
         for row in db_rows:
             self.tree.insert('', 0, text = row[1], values = row[2])
+    
+    def validation(self):
+        return len(self.name.get()) != 0 and len(self.price.get()) != 0
 
+    def add_product(self):
+        if self.validation():
+            query = 'INSERT INTO product VALUES(NULL, ?, ?)'
+            parameters = (self.name.get(), self.price.get())
+            self.run_query(query, parameters)
+            print('Data save')
+        else:
+            print('Name and price is requiered')
+        self.get_products()
 
 if __name__ == '__main__':
     window = Tk()
